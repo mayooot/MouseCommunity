117b8a7cb966cdf92c5dd7bef45c41a10d312639
Richard Li <1129972602@qq.com>
2021-12-22 16:11:44 +0800

    init repo

---
+++ src/main/java/community/controller/HelloController.java
@@ -0,0 +1,24 @@
+package community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * <p>项目文档： TODO</p>
+ *
+ * @author liming
+ * @version 1.0.0
+ * @createTime 2021年12月22日 15:44:00
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+
+    }
+}
